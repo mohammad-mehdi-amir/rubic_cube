d90cb1ca95eaf412b06dfd841c8b19637a273711
mohamad <afroginlake1@gmail.com>
2024-12-25 00:04:00 +0330

    fix rotator funtions

--- init_cube.py
+++ init_cube.py
@@ -1,14 +1,28 @@
 class Cube:
     def __init__(self, state: dict):
         self.cube = state
-
-    def generate_state_id(self):
+        self.code=self.code(self.cube)
+        self.goal_state_id="UUUUUUUUULLLLLLLLLFFFFFFFFFRRRRRRRRRBBBBBBBBBDDDDDDDDD"
+    def code(self):
         state_id = ""
-        for face in ["U", "D", "F", "B", "L", "R"]:
+        for face in ["U", "L", "F", "R", "B", "D"]:
             for row in self.cube[face]:
                 state_id += "".join(map(str, row))
         return state_id
-
+    def decode(self, state_id: str):
+        faces = ["U", "L", "F", "R", "B", "D"]
+        face_size = 3 
+        cube = {}
+        
+        index = 0
+        for face in faces:
+            self.cube[face] = []
+            for _ in range(face_size):
+                row = list(state_id[index:index + face_size])
+                self.cube[face].append(row)
+                index += face_size
+        return  cube
+    
     def rotate_face_clockwise(self, face):
         self.cube[face] = [list(row) for row in zip(*self.cube[face][::-1])]
 
@@ -49,23 +63,39 @@ class Cube:
                 self.cube["B"][2 - i][2] = self.cube["U"][i][0]
                 self.cube["U"][i][0] = temp[i]
 
-    def Rotate_F(self, direction: bool):
-        if direction:
+    def Rotate_F(self, direction=True):
+        if direction:  # Clockwise rotation
             self.rotate_face_clockwise("F")
-            temp = self.cube["U"][2]
-            self.cube["U"][2] = [self.cube["L"][2 - i][2] for i in range(3)]
+            
+            # Temporary variables for swapping edges
+            u_row = self.cube["U"][2]
+            l_col = [self.cube["L"][i][2] for i in range(3)]
+            d_row = self.cube["D"][0]
+            r_col = [self.cube["R"][i][0] for i in range(3)]
+
+            # Perform the swap in clockwise direction
+            self.cube["U"][2] = l_col[::-1]
             for i in range(3):
-                self.cube["L"][i][2] = self.cube["D"][0][i]
-                self.cube["D"][0][i] = self.cube["R"][2 - i][0]
-                self.cube["R"][i][0] = temp[i]
-        else:
+                self.cube["L"][i][2] = d_row[i]
+            self.cube["D"][0] = r_col[::-1]
+            for i in range(3):
+                self.cube["R"][i][0] = u_row[i]
+        else:  # Counterclockwise rotation
             self.rotate_face_counterclockwise("F")
-            temp = self.cube["U"][2]
-            self.cube["U"][2] = [self.cube["R"][i][0] for i in range(3)]
+            
+            # Temporary variables for swapping edges
+            u_row = self.cube["U"][2]
+            l_col = [self.cube["L"][i][2] for i in range(3)]
+            d_row = self.cube["D"][0]
+            r_col = [self.cube["R"][i][0] for i in range(3)]
+
+            # Perform the swap in counterclockwise direction
+            self.cube["U"][2] = r_col
             for i in range(3):
-                self.cube["R"][i][0] = self.cube["D"][0][2 - i]
-                self.cube["D"][0][2 - i] = self.cube["L"][i][2]
-                self.cube["L"][2 - i][2] = temp[i]
+                self.cube["R"][i][0] = d_row[::-1][i]
+            self.cube["D"][0] = l_col
+            for i in range(3):
+                self.cube["L"][i][2] = u_row[::-1][i]
 
     def Rotate_R(self, direction: bool):
         if direction:
@@ -86,22 +116,26 @@ class Cube:
                 self.cube["U"][i][2] = temp[i]
 
     def Rotate_B(self, direction: bool):
-        if direction:
+        if direction:  # Clockwise rotation
             self.rotate_face_clockwise("B")
+            
             temp = self.cube["U"][0]
-            self.cube["U"][0] = [self.cube["R"][2 - i][2] for i in range(3)]
+            self.cube["U"][0] = [self.cube["R"][i][2] for i in range(3)][::-1]
             for i in range(3):
-                self.cube["R"][i][2] = self.cube["D"][2][2 - i]
-                self.cube["D"][2][2 - i] = self.cube["L"][2 - i][0]
+                self.cube["R"][i][2] = self.cube["D"][2][i]
+            self.cube["D"][2] = [self.cube["L"][i][0] for i in range(3)][::-1]
+            for i in range(3):
                 self.cube["L"][i][0] = temp[i]
-        else:
+        else:  # Counterclockwise rotation
             self.rotate_face_counterclockwise("B")
+            
             temp = self.cube["U"][0]
             self.cube["U"][0] = [self.cube["L"][i][0] for i in range(3)]
             for i in range(3):
-                self.cube["L"][i][0] = self.cube["D"][2][i]
-                self.cube["D"][2][i] = self.cube["R"][2 - i][2]
-                self.cube["R"][2 - i][2] = temp[i]
+                self.cube["L"][i][0] = self.cube["D"][2][2 - i]
+            self.cube["D"][2] = [self.cube["R"][i][2] for i in range(3)]
+            for i in range(3):
+                self.cube["R"][i][2] = temp[2 - i]
 
     def Rotate_D(self, direction: bool):
         if direction:
@@ -118,3 +152,11 @@ class Cube:
             self.cube["R"][2] = self.cube["B"][2]
             self.cube["B"][2] = self.cube["L"][2]
             self.cube["L"][2] = temp
+    
+    
+    def is_solved(self):
+        if self.code() == self.goal_state_id:
+            return True
+        else:
+            return False
+       
--- test_cube.py
+++ test_cube.py
@@ -1,14 +1,46 @@
 from init_cube import Cube
-initial_state = {
-    "U": [['U', 'U', 'U'], ['U', 'U', 'U'],['U', 'U', 'U']],
-    "D": [['D', 'D', 'D'], ['D', 'D', 'D'], ['D', 'D', 'D']],
-    "F": [['F', 'F', 'F'], ['F', 'F', 'F'], ['F', 'F', 'F']],
-    "B": [['B', 'B', 'B'], ['B', 'B', 'B'], ['B', 'B', 'B']],
-    "L": [['L', 'L', 'L'], ['L', 'L', 'L'], ['L', 'L', 'L']],
-    "R": [['R', 'R', 'R'], ['R', 'R', 'R'], ['R', 'R', 'R']],
+# initial_state = {
+#     'U': [['R', 'U', 'U'],
+#           ['F', 'U', 'U'],
+#           ['L', 'L', 'L']],
+
+#     'L': [['F', 'L', 'B'],
+#           ['F', 'L', 'D'],
+#           ['F', 'F', 'R']],
+
+#     'F': [['D', 'D', 'D'],
+#           ['F', 'F', 'R'],
+#           ['U', 'R', 'R']],
+
+#     'R': [['F', 'B', 'B'],
+#           ['U', 'R', 'R'],
+#           ['B', 'B', 'U']],
+
+#     'B': [['L', 'L', 'U'],
+#           ['B', 'B', 'U'],
+#           ['F', 'L', 'D']],
+
+#     'D': [['F', 'D', 'D'],
+#           ['R', 'D', 'D'],
+#           ['R', 'B', 'L']],
+
+# }
+g_state = {
+    "U": [
+        ['U', 'U', 'U'],['U', 'U', 'U'],['U', 'U', 'U']],
+    "D": [
+        ['D', 'D', 'D'],['D', 'D', 'D'],['D', 'D', 'D']],
+    "F": [
+        ['F', 'F', 'F'],['F', 'F', 'F'],['F', 'F', 'F']],
+    "B": [
+        ['B', 'B', 'B'],['B', 'B', 'B'],['B', 'B', 'B']],
+    "L": [
+        ['L', 'L', 'L'],['L', 'L', 'L'],['L', 'L', 'L']],
+    "R": [
+        ['R', 'R', 'R'],['R', 'R', 'R'],['R', 'R', 'R']]
 }
 
-cube = Cube(initial_state)
+
 
 def print_cube(cube):
     for face, grid in cube.cube.items():
@@ -17,21 +49,73 @@ def print_cube(cube):
             print(row)
     print("\n")
 
-print("Initial State:")
-print_cube(cube)
+cube = Cube(g_state)
+# print(cube.code())
+z=cube
+cube.Rotate_D(direction=True)
+print(z.code())
+# cube.Rotate_R(direction=True)
+# cube.Rotate_B(direction=False)
+# cube.Rotate_F(direction=True)
 
-print("Rotate U clockwise:")
-cube.Rotate_U(direction=True)
-print_cube(cube)
+# cube.Rotate_F(direction=False)
+# cube.Rotate_B(direction=True)
+# cube.Rotate_R(direction=False)
+# cube.Rotate_D(direction=False)
 
-print("Rotate L counterclockwise:")
-cube.Rotate_L(direction=False)
-print_cube(cube)
 
-print("Rotate F clockwise:")
-cube.Rotate_F(direction=True)
-print_cube(cube)
+# # cube.Rotate_F(direction=True)
+# # cube.Rotate_F(direction=True)
+# print(cube.is_solved())
+# # print(cube.code())
 
-print("Rotate D counterclockwise:")
-cube.Rotate_D(direction=False)
-print_cube(cube)
+
+
+
+
+
+
+
+
+
+# # print("Initial State:")
+# # print_cube(cube)
+
+# print("Rotate U clockwise:")
+# # cube.Rotate_U(direction=True)
+# # print_cube(cube)
+
+# print("Rotate L counterclockwise:")
+# # cube.Rotate_L(direction=False)
+# # print_cube(cube)
+
+# print("Rotate F clockwise:")
+# # cube.Rotate_F(direction=True)
+# # print_cube(cube)
+
+# print("Rotate D counterclockwise:")
+# # cube.Rotate_D(direction=False)
+# # print_cube(cube)
+
+
+# print('#-------------')
+
+# print("Rotate D counterclockwise:")
+# cube.Rotate_D(direction=True)
+# # print_cube(cube)
+
+# print("Rotate F clockwise:")
+# cube.Rotate_F(direction=False)
+# # print_cube(cube)
+
+# print("Rotate L counterclockwise:")
+# cube.Rotate_L(direction=True)
+# # print_cube(cube)
+
+# print("Rotate U clockwise:")
+# cube.Rotate_U(direction=False)
+# # print_cube(cube)
+
+# print(cube.code())
+# print(cube.goal_state_id)
+# print(cube.is_solved())
